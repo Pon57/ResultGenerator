44d2255fcda521a65699459fcc49ed90d35160ab
Pon <tyanpon57@gmail.com>
2018-08-27 05:52:29 +0900

    Get information from Challonge API and visualize

--- index.php
+++ index.php
@@ -1,22 +1,85 @@
 <?php
 
-	include('challonge.class.php');
-	$c = new ChallongeAPI('');
-	$c->verify_ssl = false;
-	$t = $c->getParticipants('karisuma20_A');
-	$result = [];
-	$i = 0;
-	foreach($t as $key => $s){
-		$result[$i]['id'] = (integer)$s->id;
-		$result[$i]['name'] = (string)$s->name;
-		$result[$i]['final-rank'] = (integer)$s->{'final-rank'};
-		$i++;
-	}
-	sortArrayByKey($result,'final-rank');
+	declare(strict_types=1);
+	//require
+	$autoloader = require_once __DIR__.'/vendor/autoload.php';
 	
-	print_r($result);
+	/*$result = $autoloader->findFile('Api\ChallongeAPI');
+	echo json_encode($result) . "\n";*/
 
-	function sortArrayByKey(array &$array, string $sortKey, int $sortType = SORT_ASC) {
+    //for .env
+	use Dotenv\Dotenv;
+	
+	$dotenv = new Dotenv(__DIR__);
+	$dotenv->load();
+	$challonge_api = getenv('CHALLONGE_API');
+
+	use API\ChallongeAPI;
+
+	$challonge = new ChallongeAPI($challonge_api);
+	$challonge->verify_ssl = false;
+	$tournament_name = 'toryumon_31';
+	$tournament = $challonge->getTournament($tournament_name);
+	$participants = $challonge->getParticipants($tournament_name);
+	$matches = $challonge->getMatches($tournament_name, ['state'=>'complete']);
+
+	
+	$results = [];
+	foreach($participants as $key => $participant){
+		$results[(integer)$participant->id]['name'] = (string)$participant->name;
+		/*if((integer)$participant->{'final-rank'} > 0){
+			$results[(integer)$participant->id]['rank'] = (integer)$participant->{'final-rank'};
+		} else {
+			$results[(integer)$participant->id]['rank'] = '?';
+			$results[(integer)$participant->id]['in-winner'] = 1;
+			$results[(integer)$participant->id]['lost-loser'] = 0;
+		}*/
+		$results[(integer)$participant->id]['rank'] = '?';
+		$results[(integer)$participant->id]['in-winner'] = 1;
+		$results[(integer)$participant->id]['lost-loser'] = 0;
+	}
+
+	//if((string)$tournament->state !== 'complete' && $matches) {
+	if($matches) {
+		$rounds = [];
+		foreach($matches as $m_key => $match){
+			if($results[(integer)$match->{'loser-id'}]['in-winner'] === 0){
+				$results[(integer)$match->{'loser-id'}]['lost-loser'] = (integer)$match->round;
+				if((integer)$match->round > 0){
+					$results[(integer)$match->{'loser-id'}]['lost-winner'] = (integer)$match->round;
+					$results[(integer)$match->{'winner-id'}]['rank'] = 1;
+					$results[(integer)$match->{'loser-id'}]['rank'] = 2;
+				} else {
+					$rounds[] = (integer)$match->round;
+				}
+			} else {
+				$results[(integer)$match->{'loser-id'}]['lost-winner'] = (integer)$match->round;
+				$results[(integer)$match->{'loser-id'}]['in-winner'] = 0;
+			}
+		}
+		sortArrayByKey($results,'lost-loser',SORT_DESC);
+		$rounds = array_count_values($rounds);
+		$pre_round = 0;
+		$sum_count = 0;
+		
+		foreach($results as $id => $result){
+			if($result['lost-loser'] < 0){
+				if($pre_round !== $result['lost-loser']){
+					$pre_round = $result['lost-loser'];
+					$sum_count += $rounds[$result['lost-loser']];
+				}
+				if($result['lost-loser'] > -4){
+					unset($results[$id]);
+					continue;
+				}
+				$results[$id]['rank'] = $tournament->{'participants-count'} - $sum_count + 1;
+			}
+		}
+	}
+	sortArrayByKey($results,'rank');
+	$height = 50 * count($results);
+
+	function sortArrayByKey(array &$array, string $sortKey, int $sortType = SORT_ASC): void {
 		$tmp_array = [];
 		foreach ($array as $key => $row) {
 			$tmp_array[$key] = $row[$sortKey];
@@ -24,7 +87,13 @@
 		array_multisort($tmp_array, $sortType, $array);
 		unset($tmp_array);
 	}
+
+	function json_safe_encode(array $data): string {
+		return json_encode($data, JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT);
+	}
 ?>
+
+
 
 <!DOCTYPE html>
 <html>
@@ -33,6 +102,7 @@
 <title>ResultGenerator</title>
 <meta name="Keywords" content=" ">
 <meta name="Description" content=" ">
+<meta name="robots" content="noindex,nofollow">
 
 <!-- スタイルは特に影響無いです -->
 <style type="text/css">
@@ -76,7 +146,9 @@
 <!-- 今回はEaselJSのみ使用します-->
 <script src="https://code.createjs.com/easeljs-0.8.2.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="my.js"></script>
+<script id="script" type="text/javascript" src="my.js"
+ data-param='<?php echo json_safe_encode($results);?>'>
+</script>
 
 </head>
 <body>
@@ -88,12 +160,13 @@
 		<h2>入力してください</h2>
 		<dl class="table">
 			<dt>タイトル</dt>
-			<dd><input type="text" name="title" id="title" placeholder="Umebura 20" value="Umebura 20" /></dd>
+			<dd><input type="text" name="title" id="title" placeholder="大会名" value="<?= $tournament->name ?>" /></dd>
 			<dt>情報</dt>
 			<dd><input type="text" name="info" id="info" placeholder="80 entrants - Aichi, Chubu (Japan) [8/4]" value="80 entrants - Aichi, Chubu (Japan) [8/4]" /></dd>
-			<dt>テスト</dt>
-			<dd><input type="text" name="test" id="test" placeholder="80 entrants - Aichi, Chubu (Japan) [8/4]" value="1." /></dd>
-			<dd><input type="text" name="test2" id="test2" placeholder="80 entrants - Aichi, Chubu (Japan) [8/4]" value="2." /></dd>
+			<dt>参加者</dt>
+<?php	  foreach($results as $key => $value){ ?>
+			<dd><input type="text" name="player<?= $key ?>" id="player<?= $key ?>" placeholder="1" size="2" value="<?= $value['rank'] ?>" /><input type="text" name="player<?= $key ?>_name" id="player<?= $key ?>_name" placeholder="1" value="<?= $value['name'] ?>" /></dd>
+<?php	  } ?>
 		</dl>
 		<div class="btn">
 			<button id="update">画像生成</button>
@@ -104,7 +177,7 @@
 		<h2>結果</h2>
 		<div class="canvas">
 			<!-- width,heightは必須 -->
-			<canvas id="result" width="427" height="1145"></canvas>
+			<canvas id="result" width="427" height="<?= 194+$height+30 ?>"></canvas>
 		</div>
 		<div class="btn">
 			<form action="img.php" method="post" id="saveform">
--- my.js
+++ my.js
@@ -1,24 +1,25 @@
 (function($){
 	//初期化
 	var init_canvas = function(){
-		
+		var $script = $('#script');
+		results = JSON.parse($script.attr('data-param'));
+		//確認
 		logoImg = new Image();
-		logoImg.src = 'umebura.png';
+		logoImg.src = 'karisuma.png';
+		stage = new createjs.Stage('result');
 		jpImg = new Image();
 		jpImg.src = 'jp.png';
-		stage = new createjs.Stage('result');
-
 		bg_shape = new createjs.Shape();//shapeインスタンスを作る
 		bg_shape.graphics.f("white").dr(0,0,stage.canvas.width,stage.canvas.height);//形を白い四角に決める、幅と高さはステージと同サイズにする
 		genImage();
 		stage.update();
 	}
 
-	var strWidth = function(str) {
+	var strWidth = function(str, font) {
 		var canvas = document.getElementById('result');
 		if (canvas.getContext) {
 		  var context = canvas.getContext('2d');
-		  context.font = "12px";
+		  context.font = font;
 		  var metrics = context.measureText(str);
 		  return metrics.width;
 		}
@@ -29,8 +30,13 @@
 	var genImage = function(){
 		var logo = new createjs.Bitmap(logoImg);
 
-		logo.x = stage.canvas.width/2-logoImg.width/2;
-		logo.y = 10;
+		logo.scaleY = 120 / logo.getBounds().height;
+		logo.scaleX = logo.scaleY;
+		
+		logo.x = stage.canvas.width/2-logoImg.width/2*logo.scaleX;
+		logo.y = 30;
+
+		
 		
 		stage.addChild(bg_shape);//白い四角をステージに先に置く
 		stage.addChild(logo);
@@ -40,70 +46,113 @@
 		var txt = {
 			'title' : {
 				'x' : stage.canvas.width/2,	
-				'y' : tmp=logoImg.height+20,
-				'size': '15px',
+				'y' : tmp=logoImg.height*logo.scaleY+logo.y+10,
+				'size': '16px',
 				'align': 'center'
 			},
 			'info' : {
 				'x' : stage.canvas.width/2,
-				'y' : tmp+=25,
-				'size': '15px',
+				'y' : tmp+=16+5,
+				'size': '13px',
 				'align': 'center'
 			}
 		}
-		txt["test"] = {
-			'x' : 30,
-			'y' : 141+40,
-			'size': '15px',
-			'align': 'left'
-		};
-		txt["test2"] = {
-			'x' : 30,
-			'y' : txt["test"]["y"]+50,
-			'size': '15px',
-			'align': 'left'
-		};
-		var tmp_y = 20;
-		//上記の配列より文字オブジェクトを生成し、stageにaddChildする
+
+		var result_y = tmp;
+		$.each(results,function(key,value){
+			txt["player"+key] = {
+				'x' : 50,
+				'y' : result_y+=50,
+				'size': '20px',
+				'align': 'left'
+			};
+		});
+		var tmp_y = 13 + 21;
+		//上記の配列より文字オブジェクトを生成し、stageにaddChildする   194
 		$.each(txt,function(key,value){
 			//本文は入力された内容をそのまま取る
 			var content = $('#' + key).val();
-
-			if(key != 'title' && key != 'info'){
-				var region = new createjs.Bitmap(jpImg);
-				region.x = 45
-				region.y = 141+tmp_y;
-				tmp_y += 50;
-				stage.addChild(region);
-			}
 		
 			//文字オブジェクト生成
+			if(key != 'title' && key != 'info'){
+				content = content + '.';
+			}
 			var obj = new createjs.Text(content);
+
+			if(key == 'info'){
+				style = 'italic';
+			} else {
+				style = 'bold';
+			}
 			
 			//文字の属性を設定する
 			obj.textAlign = value.align;
-			obj.font = 'bold ' + value.size + '/1.5 Meiryo,sans-serif';  //CSSのfontのショートハンドと同じ
+			obj.font = style + ' ' + value.size + '/1.5 Yu Gothic,sans-serif';  //CSSのfontのショートハンドと同じ
 			obj.x = value.x; //X座標
 			obj.y = value.y; //Y座標
+
+			if(key != 'title' && key != 'info'){
+				obj.x -= strWidth(content,obj.font);
+				var region = new createjs.Bitmap(jpImg);
+				//region.x = 45
+				region.x = obj.x+strWidth(content,obj.font);
+				region.y = tmp+tmp_y;
+				tmp_y += 50;
+				stage.addChild(region);
+
+				//本文は入力された内容をそのまま取る
+				var name = $('#' + key + '_name').val();
+			
+				//文字オブジェクト生成
+				var player = new createjs.Text(name);
+
+				//文字の属性を設定する
+				player.textAlign = 'left';
+				player.font = 'bold 20px/1.5 Yu Gothic,sans-serif';  //CSSのfontのショートハンドと同じ
+				player.x = region.x+jpImg.width+10; //X座標
+				player.y = obj.y; //Y座標
+				stage.addChild(player);
+
+				var charImg = new Image();
+				charImg.src = 'stockicons/bayonetta_01.png';
+				
+				var character = new createjs.Bitmap(charImg);
+				character.scaleY = (jpImg.height+10) / character.getBounds().height;
+				character.scaleX = character.scaleY;
+				character.x = player.x+strWidth(name,obj.font)-2;
+				character.y = region.y-5;
+				stage.addChild(character);
+				
+
+				var charImg2 = new Image();
+				charImg2.src = 'stockicons/captain_03.png';
+				var character2 = new createjs.Bitmap(charImg2);
+				character2.scaleY = (jpImg.height+10) / character2.getBounds().height;
+				character2.scaleX = character.scaleY;
+				character2.x = character.x+charImg2.width*character2.scaleX-3;
+				character2.y = character.y;
+				stage.addChild(character2);
+
+			}
 			
 			stage.addChild(obj);
 		});
-		}
+	}
 
 		
-		//保存させる処理
-		//ここだけはブラウザ上で完結できなかった…。
-		//base64をデコードしてheaderを付加してechoするだけのPHPへPOSTしています
-		var save = function(){
-			var png = stage.toDataURL('image/png'); //base64エンコードした画像データ生成
-			if(png){
-				$('input[name="img"]').remove();
-				$('#saveform').append('<input type="hidden" name="img" value="'+png+'" />');
-				return true;
-			} else {
-				return false;
-			}
+	//保存させる処理
+	//ここだけはブラウザ上で完結できなかった…。
+	//base64をデコードしてheaderを付加してechoするだけのPHPへPOSTしています
+	var save = function(){
+		var png = stage.toDataURL('image/png'); //base64エンコードした画像データ生成
+		if(png){
+			$('input[name="img"]').remove();
+			$('#saveform').append('<input type="hidden" name="img" value="'+png+'" />');
+			return true;
+		} else {
+			return false;
 		}
+	}
 	
 	$(function(){
 		//canvasオブジェクトを事前に定義しておく
