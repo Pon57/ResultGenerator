91524190b937cbb5e7a3cd88b1f83b9923226f42
Pon <tyanpon57@gmail.com>
2018-11-29 05:20:48 +0900

    Update to PSR compliant code.

--- Classes/Functions.php
+++ Classes/Functions.php
@@ -1,22 +1,24 @@
 <?php
-	
-	namespace Classes;
-	
-	/**
-	* 使う関数持ってるクラス
-	*/
-	class Functions {
-	
-		public function __construct() {
-		}
-		
-		public function sortArrayByKey(array &$array, string $sortKey, int $sortType = SORT_ASC): void {
-		    $tmp_array = [];
-		    foreach ($array as $key => $row) {
-		        $tmp_array[$key] = $row[$sortKey];
-		    }
-		    array_multisort($tmp_array, $sortType, $array);
-		    unset($tmp_array);
-		}
-	
-	}
+
+namespace Classes;
+
+/**
+* 使う関数持ってるクラス
+*/
+class Functions
+{
+
+    public function __construct()
+    {
+    }
+
+    public function sortArrayByKey(array &$array, string $sortKey, int $sortType = SORT_ASC): void
+    {
+        $tmp_array = [];
+        foreach ($array as $key => $row) {
+            $tmp_array[$key] = $row[$sortKey];
+        }
+        array_multisort($tmp_array, $sortType, $array);
+        unset($tmp_array);
+    }
+}
--- index.php
+++ index.php
@@ -1,46 +1,46 @@
 <?php
 
-	declare(strict_types=1);
-	
-	use Dotenv\Dotenv;
-	use API\SmashggAPI;
-	use API\ChallongeAPI;
-	use Classes\Challonge;
-	use Classes\Smashgg;
-	use Classes\Functions;
-	
-	//require
-	$autoloader = require_once __DIR__.'/vendor/autoload.php';
-	
-	$url = 'https://challonge.com/toryumon_40';
-	$url_array = explode('/', $url);
-	
-	if (isset($url_array[2])) {
-		
-		switch ($url_array[2]) {
-			case 'challonge.com':
-				$dotenv = new Dotenv(__DIR__);
-				$dotenv->load();
-				$challonge_api = getenv('CHALLONGE_API');
-				
-				$tournament = new Challonge($challonge_api, $url);
-				break;
-			case 'smash.gg':
-				$tournament = new Smashgg($url);
-				break;
-		}
-		$results = $tournament->getResultsList();
-		$title = $tournament->getTournamentTitle();
-		$total_entrants = $tournament->getTotalEntrants();
-	}
-	
-	$functions = new Functions();
-	$functions->sortArrayByKey($results, 'rank');
-	
-	$height = 50 * count($results);
-	
-	function json_safe_encode(array $data): string {
-	    return json_encode($data, JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT);
-	}
-	
-	include('form.html');
+declare(strict_types=1);
+
+use Dotenv\Dotenv;
+use API\SmashggAPI;
+use API\ChallongeAPI;
+use Classes\Challonge;
+use Classes\Smashgg;
+use Classes\Functions;
+
+//require
+$autoloader = require_once __DIR__.'/vendor/autoload.php';
+
+$url = 'https://challonge.com/toryumon_40';
+$url_array = explode('/', $url);
+
+if (isset($url_array[2])) {
+    switch ($url_array[2]) {
+        case 'challonge.com':
+            $dotenv = new Dotenv(__DIR__);
+            $dotenv->load();
+            $challonge_api = getenv('CHALLONGE_API');
+
+            $tournament = new Challonge($challonge_api, $url);
+            break;
+        case 'smash.gg':
+            $tournament = new Smashgg($url);
+            break;
+    }
+    $results = $tournament->getResultsList();
+    $title = $tournament->getTournamentTitle();
+    $total_entrants = $tournament->getTotalEntrants();
+}
+
+$functions = new Functions();
+$functions->sortArrayByKey($results, 'rank');
+
+$height = 50 * count($results);
+
+function json_safe_encode(array $data): string
+{
+    return json_encode($data, JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT);
+}
+
+include('form.html');
